32397c0e5d1a5b67bf5ff8702473bac29e4ab723
Trewas345 <vazheninsaha@gmail.com>
2024-08-11 19:33:51 +0500

    1.1.5

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,49 +1,33 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC;
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
 
-    {
-        try {
-            userDaoJDBC = new UserDaoJDBCImpl();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public void createUsersTable() {
+        userDaoHibernate.createUsersTable();
     }
 
-    public UserServiceImpl()  {
+    public void dropUsersTable() {
+        userDaoHibernate.dropUsersTable();
     }
 
-    public void createUsersTable() throws SQLException {
-        userDaoJDBC.createUsersTable();
+    public void saveUser(String name, String lastName, byte age) {
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
-    public void dropUsersTable() throws SQLException {
-        userDaoJDBC.dropUsersTable();
+    public void removeUserById(long id) {
+        userDaoHibernate.removeUserById(id);
     }
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDaoJDBC.saveUser(name,lastName,age);
-    }
-
-    public void removeUserById(long id) throws SQLException {
-        userDaoJDBC.removeUserById(id);
-    }
-
-    public List<User> getAllUsers() throws SQLException {
-
-        return userDaoJDBC.getAllUsers();
+    public List<User> getAllUsers() {
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,21 +3,45 @@ package jm.task.core.jdbc.util;
 
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 
 
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        String URL = "jdbc:mysql://localhost:3306/mydbtest";
-        String NAME = "root";
-        String PASSWORD = "root";
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(URL, NAME, PASSWORD);
+    public static SessionFactory getConnection()  {
+        final String driver = "com.mysql.cj.jdbc.Driver";
+        final String host = "jdbc:mysql://localhost:3306/mydbtest";
+        final String login = "root";
+        final String password = "root";
+        SessionFactory sessionFactory = null;
+
+
+            try {
+                Configuration configuration = new Configuration()
+                        .setProperty("hibernate.connection.driver_class", driver)
+                        .setProperty("hibernate.connection.url", host)
+                        .setProperty("hibernate.connection.username", login)
+                        .setProperty("hibernate.connection.password", password)
+                        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                        .addAnnotatedClass(User.class)
+                        .setProperty("hibernate.c3p0.min_size","5")
+                        .setProperty("hibernate.c3p0.max_size","200")
+                        .setProperty("hibernate.c3p0.max_statements","200");
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (HibernateException e) {
+                e.printStackTrace();
+            }
+            return sessionFactory;
     }
 
 }
